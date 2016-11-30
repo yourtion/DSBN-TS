7c2c6d9fc435971bbb48d44b19908fea721bd9b2
YourtionGuo <yourtion@gmail.com>
2016-11-30 09:50:55 +0800

    add package and test

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-import { generator } from "./generator";
-import { lexer, parser } from "./parser";
-import { transformer } from "./transformer";
-import { dump } from "./utils";
-
-const lex = lexer("Paper 100");
-dump(lex);
-
-const parsed = parser(lex);
-dump(parsed);
-
-const transformed = transformer(parsed);
-dump(transformed);
-
-const xml = generator(transformed);
-dump(xml);
+export { generator } from "./generator";
+export { lexer, parser } from "./parser";
+export { transformer } from "./transformer";
